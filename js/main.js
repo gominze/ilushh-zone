8fda8fdc6e01a1db824ea9933b38bdc0a33c1cda
ilushh <118263026+ilushh@users.noreply.github.com>
2023-02-07 23:06:08 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+// Speichern
 const form = document.querySelector("form");
+
+// Ergebnis zeigen
 const result = document.querySelector("#result");
 
+// Wenn das Formular gesendet wird, wird eine Funktion ausgeführt, die den Wert des Formulars berechnet.
 form.addEventListener("submit", event => {
   event.preventDefault();
+
+// Durchschnitt der 5 Fragen
 
   const question1 = parseInt(document.querySelector("#question1").value, 10);
   const question2 = parseInt(document.querySelector("#question2").value, 10);
@@ -12,13 +18,15 @@ form.addEventListener("submit", event => {
 
   const average = (question1 + question2 + question3 + question4 + question5) / 5;
 
-  result.textContent = `Der aktuelle Stimmungsindex beträgt: ${average}`;
+  result.textContent = `Dein aktuelle Stimmungsindex beträgt: ${average}`;
 });
 
 // Überprüfen Sie, ob das Formular abgesendet wurde
 document.querySelector("#mood-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    // Berechnen Sie den Stimmungsindex
+
+// Berechnen Sie den Stimmungsindex
     const question1 = parseInt(document.querySelect)
 });
+
