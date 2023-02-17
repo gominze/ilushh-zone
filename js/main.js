0b2bced2f8b31a324b4e21906bfa376285203d94
ilushh <118263026+ilushh@users.noreply.github.com>
2023-02-17 15:36:12 +0100

    Abgabe von Stimmen einzeln ergebnis und gesamt

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-// Speichern
+/* Speichern
+"form" wurde auf HTML zuvor definiert
+"querySelectorAll" gibt eine Liste von Elementen  zurück was auf CSS sich bezieht
+".slider" ist der CSS-Selektor, der Elemente auswählt, die die Klasse "slider" haben.*/
+
 const form = document.querySelector("#survey-form");
 const sliders = form.querySelectorAll(".slider");
+const result = document.querySelector("#result"); // Für einzelne Teilnehmer
+
+const results = [];
+
+
+/*Code snippetr hat die Aufgabe für jedes Input-Element in der in der Variable "sliders" 
+gespeicherten Liste von HTML-Elementen, einen Event-Listener zu registrieren, 
+der auf das "input"-Event reagiert.*/
 
 sliders.forEach(slider => {
   slider.addEventListener("input", function() {
@@ -10,21 +22,34 @@ sliders.forEach(slider => {
   });
 });
 
-// Ergebnis zeigen
-const result = document.querySelector("#result");
-
 // Wenn das Formular gesendet wird, wird eine Funktion ausgeführt, die den Wert des Formulars berechnet.
 form.addEventListener("submit", event => {
   event.preventDefault();
 
-  // Durchschnitt der 5 Fragen
-  const question1 = parseInt(document.querySelector("#question1").value, 10);
-  const question2 = parseInt(document.querySelector("#question2").value, 10);
-  const question3 = parseInt(document.querySelector("#question3").value, 10);
-  const question4 = parseInt(document.querySelector("#question4").value, 10);
-  const question5 = parseInt(document.querySelector("#question5").value, 10);
+// Durchschnitt der 5 Fragen
+const question1 = parseInt(document.querySelector("#question1").value, 10);
+const question2 = parseInt(document.querySelector("#question2").value, 10);
+const question3 = parseInt(document.querySelector("#question3").value, 10);
+const question4 = parseInt(document.querySelector("#question4").value, 10);
+const question5 = parseInt(document.querySelector("#question5").value, 10);
 
-  const average = (question1 + question2 + question3 + question4 + question5) / 5;
+const average = (question1 + question2 + question3 + question4 + question5) / 5;
 
-  result.textContent = `Dein aktuelle Stimmungsindex beträgt: ${average}`;
+// fügt den durchschnittlichen Stimmungsindex des aktuellen Formulars in das results-Array ein
+  results.push(average);
+
+
+// Durchschnittsbewertung jedes Benutzers als Teil der Gesamtbewertungen in einem Ergebnisfeld angezeigt.
+  result.innerHTML += `Teilnehmer ${results.length}: ${average}<br>`;
+
+// nach 20 Eingaben werden die Einträge gelöscht
+  form.reset();
+
+
+// wenn 20 Stimmen abgegeben sind wird der Stimungsindex ausgerechnet.
+  if (results.length === 20) {
+    // kalkulieren
+    const overallAverage = results.reduce((sum, result) => sum + result, 0) / results.length;
+    result.innerHTML += `Der durchschnittliche Stimmungsindex aller Teilnehmer beträgt: ${overallAverage}`;
+  }
 });
